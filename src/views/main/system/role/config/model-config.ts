cb1001fe9408737464b3cff4c3c6661556e03492
chenguo6666 <cg1305378470@163.com>
2022-01-06 21:40:06 +0800

    feat: 用户管理的弹框

---
+++ src/views/main/system/role/config/model-config.ts
@@ -0,0 +1,27 @@
+export const modelConfig = {
+  formItems: [
+    {
+      field: 'name',
+      type: 'input',
+      label: '角色名',
+      placeholder: '请输入角色名'
+    },
+    {
+      field: 'intro',
+      label: '角色介绍',
+      type: 'input',
+      placeholder: '请输入角色介绍'
+    }
+  ],
+  colLayout: {
+    xl: 24,
+    lg: 24,
+    md: 24,
+    sm: 24,
+    xs: 24
+  },
+  labelWidth: '120px',
+  itemStyle: {
+    padding: '0px 40px'
+  }
+};
--- src/views/main/system/role/role.vue
+++ src/views/main/system/role/role.vue
@@ -4,26 +4,45 @@
     <page-content
       :contentTableConfig="contentTableConfig"
       pageName="role"
+      @newBtnClick="handleNewData"
+      @editBtnClick="handleEditData"
     ></page-content>
+    <page-model
+      :defaultInfo="defaultInfo"
+      :modelConfig="modelConfig"
+      pageName="role"
+      ref="pageModelRef"
+    ></page-model>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import PageModel from '@/components/page-model';
+import { modelConfig } from './config/model-config';
 import PageContent from '@/components/page-content';
 import PageSearch from '@/components/page-search';
 import { contentTableConfig } from './config/content-config';
 import { formConfig } from './config/search.config';
+import { usePageModel } from '@/hooks/usePageModel';
 export default defineComponent({
   name: 'role',
   components: {
     PageContent,
-    PageSearch
+    PageSearch,
+    PageModel
   },
   setup() {
+    const [pageModelRef, defaultInfo, handleEditData, handleNewData] =
+      usePageModel();
     return {
       contentTableConfig,
-      formConfig
+      formConfig,
+      modelConfig,
+      pageModelRef,
+      defaultInfo,
+      handleEditData,
+      handleNewData
     };
   }
 });
